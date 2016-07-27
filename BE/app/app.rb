066f892b796bf45864c7e91946b39c64f814e6e6
jgeliberte <jgeliberte>
2016-07-27 13:02:27 +0800

    Added validation in email and fixed the viewport for mobile

--- BE/app/app.rb
+++ BE/app/app.rb
@@ -12,19 +12,21 @@ module Kakikas
 
 		def self.send_order_and_feedback(email_or_contact,message_or_order)
 			result = false
-			begin
-				mail = Mail.deliver do
-					to 'decsneaky@gmail.com'
-					from 'Ka kika syrup'
-					subject 'Coconut Jam Orders'
-					html_part do
-						content_type 'text/html; charset=UTF-8'
-						body '<b>Ordered by :'<< email_or_contact<<' </b> <br> <b>Orders: '<< message_or_order <<'</b>' 
+			if email_or_contact != '' && message_or_order != ''
+				begin
+					mail = Mail.deliver do
+						to 'decsneaky@gmail.com'
+						from 'Ka kika syrup'
+						subject 'Coconut Jam Orders'
+						html_part do
+							content_type 'text/html; charset=UTF-8'
+							body '<b>Ordered by :'<< email_or_contact<<' </b> <br> <b>Orders: '<< message_or_order <<'</b>' 
+						end
 					end
+					result = true
+				rescue
+				puts 'Invalid Email/Contact # or Empty Feedback/Order'
 				end
-				result = true
-			rescue
-				#Catched Error
 			end
 			return result
 		end
